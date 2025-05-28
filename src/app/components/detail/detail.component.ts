1a685b02e8f2142bbf8f29f001f525086e1e9ddf
Alex Fonte <aramosdf@gmail.com>
2025-05-28 23:16:01 +0200

    fixin list and cards

--- src/app/components/detail/detail.component.ts
+++ src/app/components/detail/detail.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CharacterDTO} from "../../models/character.interface";
+import {ActivatedRoute, Router} from "@angular/router";
+import {RickAndMortyService} from "../../services/rickandmorty.service";
 
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
   styleUrl: './detail.component.css'
 })
-export class DetailComponent {
+export class DetailComponent implements OnInit
+{
+  character!: CharacterDTO;
 
+  constructor(
+    private rickAndMortyService : RickAndMortyService,
+    private activateRoute: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    const identifier = this.activateRoute.snapshot.paramMap.get('id');
+    console.log(`Id: ${identifier}`);
+
+    if (!identifier) {
+      this.router.navigateByUrl('/');
+      return;
+    }
+
+    this.rickAndMortyService.getCharactersById(identifier).subscribe((character: CharacterDTO) => {
+      if (!character) {
+        this.router.navigateByUrl('/');
+        return;
+      }
+      this.character = character;
+      console.log('Image:', this.character);
+    });
+  }
 }
